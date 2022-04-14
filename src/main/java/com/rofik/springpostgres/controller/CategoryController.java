583552427e2dc8e117aa7a6d270139501e9d28df
rofik <rofiksupriant@gmail.com>
2022-04-14 21:54:43 +0700

    update

--- src/main/java/com/rofik/springpostgres/controller/CategoryController.java
+++ src/main/java/com/rofik/springpostgres/controller/CategoryController.java
@@ -3,7 +3,6 @@ package com.rofik.springpostgres.controller;
 import com.rofik.springpostgres.domain.dto.CategoryDto;
 import com.rofik.springpostgres.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/v1/category", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value = "/v1/category")
 public class CategoryController {
 
     @Autowired
--- src/main/java/com/rofik/springpostgres/controller/ProductController.java
+++ src/main/java/com/rofik/springpostgres/controller/ProductController.java
@@ -5,17 +5,11 @@ import com.rofik.springpostgres.domain.dto.ProductListDto;
 import com.rofik.springpostgres.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "/v1/product", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value = "/v1/product")
 public class ProductController {
 
     @Autowired
